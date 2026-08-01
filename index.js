f3af20bc2bc5fe618fa3ec10e584861427a72a10
BrunoFrad <brunfradrib@gmail.com>
2026-08-01 20:39:56 -0300

    feat: create the base cli

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Just a test!")
+import { program } from "commander";
+import {asciiLogo} from "./logo.js";
+
+program
+    .name("seraphim")
+    .description("A CLI for spamming and managing spyware")
+    .version("\n\n" + asciiLogo + "\n\t\t\t\t\t\t\t\tVersion 0.0.1\n");
+
+program.parse()
