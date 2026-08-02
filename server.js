6e3715ba90bb6a3445922545afca63d1933096b8
Danielaredes1 <161955423+Danielaredes1@users.noreply.github.com>
2026-08-02 13:07:13 -0300

    feat: server que baixa um arquivo local ao acessar uma url

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from "express";
+import path from 'node:path'
 
 const PORT = process.argv[2];
 const server = express()
 
-server.get('/', (req, res) => {
-    res.send("Server running");
+server.get('/download', (req, res) => {
+    const caminho = path.join(
+        process.cwd(),
+        "public",
+        "image.png"
+    );
+
+    res.download(caminho);
 });
 
 server.listen(PORT);
