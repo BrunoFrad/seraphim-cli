d38d1226a47353f0e73b841104ad5899f2d574ad
BrunoFrad <brunfradrib@gmail.com>
2026-08-02 22:12:59 -0300

    feat: create the POST / route

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from "express";
 
 const PORT = process.argv[2];
+
 const server = express()
+server.use(express.json())
 
 server.get('/', (req, res) => {
     res.send("Server running");
+});
+
+server.post('/', (req, res) => {
+    res.status(200);
 });
 
 server.listen(PORT);
